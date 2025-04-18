d9814162ab396d62e83047c492a17f136fe072ab
ThisIsOz <mehmet.oguzcan.kervanci@student.hu-berlin.de>
2025-04-18 18:23:27 +0200

    final changes to homepage

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,26 @@ st.title("✈️ Airline Satisfaction Explorer")
 st.markdown("""
 Welcome to the **Airline Satisfaction Explorer** app!  
 Use the sidebar to navigate between pages and explore passenger satisfaction insights.
+
+---
+
+### 📊 About This App
+This interactive dashboard lets you explore passenger satisfaction trends using a real-world dataset.  
+Filter by class, travel type, customer type, and more.
+
+Built using:
+- **Streamlit** for frontend
+- **Pandas** for data manipulation
+- **Plotly** for interactive charts
+- **GitHub** for version control & cloud deployment
+
+---
+
+### 👨‍💻 Author
+
+Created with ❤️ (https://github.com/oguzcans)  
+
+> *Created in April 2025*  
+
+---
 """)
